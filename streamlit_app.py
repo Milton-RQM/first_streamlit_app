a959e590251911d31b2262b2d61b175e9b7e87a8
Data Scientist | Data Analyst - Milton <77172356+Milton-RQM@users.noreply.github.com>
2024-02-09 19:12:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit
 import pandas
 
 streamlit.title("My title")
-streamlit.header("My header")
-streamlit.text("My text")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
